3c3ed8764dc51b82a3d6c2ece9ad02c61f1b202d
smirnovio87 <s89376311108@gmail.com>
2024-05-03 19:14:31 +0300

    Проверка на ошибку биржы

--- crypto/site_crypto/price.py
+++ crypto/site_crypto/price.py
@@ -1,12 +1,16 @@
 
 import ccxt
 import json
+
 # "binance", "bybit", "coinex", "kraken", "bitget", "ascendex", "bequant", "bigone","binanceusdm", "bingx", "bitbns", "bitfinex"
 
-name_exchanges = ["binance", "bybit", "coinex", "kraken", "bitget", "ascendex", "bequant", "bigone","binanceusdm", "bingx", "bitbns", "bitfinex"]
+#name_exchanges = ["binance", "bybit", "coinex", "kraken", "bitget", "ascendex", "bequant", "bigone","binanceusdm", "bingx", "bitbns", "bitfinex"]
+name_exchanges = ccxt.exchanges
+name_exchanges = ['bitbank', 'binance']
+
 symbol = 'BTC/USDT'
 
-def price_result (name_exchanges):
+def price_result(name_exchanges):
     exchanges = {}
     for id in name_exchanges:
         #getattr возврашает значение атрибута по имени этого атрибута. 
@@ -14,18 +18,24 @@ def price_result (name_exchanges):
         exchange = getattr(ccxt, id)
         #Данной строкой записываем в словарь exchanges id {"binance": ccxt.binance} ну и так по всем name_exchanges
         exchanges[id] = exchange()
-    print (exchanges)
+    print(exchanges)
 
     data = {}
 
     for id, exchange in exchanges.items():
-        ticker = exchange.fetch_ticker(symbol)
+        try: 
+            ticker = exchange.fetch_ticker(symbol)
+            data[id] = ticker['last']
+        except Exception as err:
         #Записываем в словарь к ключу [id]-название биржы = значение exchange.fetch_ticker(symbol) - "last"
-        data[id] = ticker['last']
+            print(f"Unexpected {err=}, {type(err)=}")    
+            data[id] = "НЕ доступна"
+        
     return data
 
 data_json=price_result(name_exchanges)
 #Записываем в Json файл
-with open('btc_usdt_courses_12.json', 'w') as json_file:
-    json.dump(data_json, json_file, indent=4)
+with open('btc_usdt_courses_12.json', 'w', encoding="utf-8") as json_file:
+
+    json.dump(data_json, json_file, indent=4, ensure_ascii=False)
 print(data_json)
